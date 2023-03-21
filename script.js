60e1b9607d0278e8ca7b5ce2cc89c40ecd44af30
Mohamed-Saleem-M <mohamedsaleem.mep@gmail.com>
2023-03-22 00:49:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+setTimeout(() => {
+    document.getElementById("display").innerHTML = 09;
+    setTimeout(() => {
+        document.getElementById("display").innerHTML = 08;
+        setTimeout(() => {
+            document.getElementById("display").innerHTML = 07;
+            setTimeout(() => {
+                document.getElementById("display").innerHTML = 06;
+                setTimeout(() => {
+                    document.getElementById("display").innerHTML = 05;
+                    setTimeout(() => {
+                        document.getElementById("display").innerHTML = 04;
+                        setTimeout(() => {
+                            document.getElementById("display").innerHTML = 03;
+                            setTimeout(() => {
+                                document.getElementById("display").innerHTML = 02;
+                                setTimeout(() => {
+                                    document.getElementById("display").innerHTML = 01;
+                                    setTimeout(() => {
+                                        document.getElementById("display").innerHTML = "Happy Independence Day";
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
